7e81d2bb20fa5e83ae3d2353e3b4dda169d5e6f5
IVitalka <ivitalka24@gmail.com>
2021-05-11 19:01:22 +0700

    bugfix: cors

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const limiter = require('./utils/limiter');
 const generalRouter = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const options = {
+  origin: '*',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
 const { NODE_ENV, MONGO_DB } = process.env;
 const { PORT = 3000 } = process.env;
@@ -17,6 +27,7 @@ mongoose.connect(NODE_ENV === 'production' ? MONGO_DB : 'mongodb://localhost:270
 });
 
 const app = express();
+app.use('*', cors(options));
 app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
